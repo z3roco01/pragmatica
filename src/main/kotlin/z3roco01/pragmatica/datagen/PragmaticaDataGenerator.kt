9d75d1afd9f018bb47d1344262898f082b40f6df
z3roco01 <zerocool@e.email>
2024-09-02 01:13:37 -0500

    Add recipes for smelting the copper ores

--- src/main/kotlin/z3roco01/pragmatica/datagen/PragmaticaDataGenerator.kt
+++ src/main/kotlin/z3roco01/pragmatica/datagen/PragmaticaDataGenerator.kt
@@ -3,11 +3,13 @@ package z3roco01.pragmatica.datagen
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 import z3roco01.pragmatica.datagen.provider.PragmaticaModelProvider
+import z3roco01.pragmatica.datagen.provider.PragmaticaReciperProvider
 
 object PragmaticaDataGenerator : DataGeneratorEntrypoint {
 	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
 		val pack = fabricDataGenerator.createPack()
 
 		pack.addProvider(::PragmaticaModelProvider)
+		pack.addProvider(::PragmaticaReciperProvider)
 	}
 }
---
+++ src/main/kotlin/z3roco01/pragmatica/datagen/provider/PragmaticaReciperProvider.kt
@@ -0,0 +1,16 @@
+package z3roco01.pragmatica.datagen.provider
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider
+import net.minecraft.data.server.recipe.RecipeExporter
+import net.minecraft.item.Items
+import net.minecraft.recipe.book.RecipeCategory
+import net.minecraft.registry.RegistryWrapper
+import z3roco01.pragmatica.item.PragmaticaItems
+import java.util.concurrent.CompletableFuture
+
+class PragmaticaReciperProvider(dataOutput: FabricDataOutput, registriesFuture: CompletableFuture<RegistryWrapper.WrapperLookup>): FabricRecipeProvider(dataOutput, registriesFuture) {
+    override fun generate(exporter: RecipeExporter) {
+        offerSmelting(exporter, listOf(PragmaticaItems.RAW_CHALCOPYRITE, PragmaticaItems.RAW_CHRYSOCOLLA, PragmaticaItems.RAW_TETRAHEDRITE), RecipeCategory.MISC, Items.COPPER_INGOT, 0.7f, 200, "copper_ingot")
+    }
+}
